63fb89c3f925ace55738fe41ea81adb5cf6c6ec8
babah36-2026 <babah36@gmail.com>
2026-07-23 01:24:17 +0300

    Add files via upload

--- DZ_module_3.py
+++ DZ_module_3.py
@@ -1,5 +1,8 @@
 from datetime import datetime
-def get_days_from_today(input_date): 
-       
-    return (datetime.today().date() - datetime.strptime(input_date, "%Y-%m-%d").date()).days  
 
+def get_days_from_today(input_date):   
+    try:   
+        return (datetime.today().date() - datetime.strptime(input_date, "%Y-%m-%d").date()).days    
+    except (ValueError, TypeError):  
+        return ("Неправильний формат дати, введіть дату у форматі РРРР-ММ-ДД") 
+  # Example usage
